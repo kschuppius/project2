502cfd7bf758e3c9b81981dd489e7bd023363e30
kschuppius <kschuppius@gmail.com>
2023-10-31 14:19:58 -0400

    Update PolarisChip.js

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -75,7 +75,7 @@ export class PolarisChip extends LitElement {
   static get styles() {
     return css`
     :host {
-      display: inline-block;
+      display: flex;
       margin: 0 12px 12px 0;
       margin-top: 35px;
       padding-left: 9px;
